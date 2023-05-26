1a969315bc8e9123c88a5d0ad2f585c7533340a7
CarlosCipreste <carlos3215910@hotmail.com>
2023-05-26 16:16:18 -0300

    Avanço telas Cadastro

--- src/main/java/com/gragas/gragas/CadastroController.java
+++ src/main/java/com/gragas/gragas/CadastroController.java
@@ -1,35 +1,25 @@
 package com.gragas.gragas;
 
+import com.gragas.gragas.metodos.Formatacao;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 
-public class CadastroController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CadastroController implements Initializable {
 
     @FXML
-    private Pane menuPane;
+    private Pane FornecedorPane;
+
+    @FXML
+    private Pane FuncionarioPane;
 
     @FXML
     private Pane ProdutoPane;
-
-    @FXML
-    private Button voltarButton;
-
-    @FXML
-    private Button CadUsuariosButton;
-
-    @FXML
-    private Button CadClientesButton;
-
-    @FXML
-    private Button CadFornecedorButton;
-
-    @FXML
-    private Button CadProdutosButton;
 
     @FXML
     private CheckBox alcoolicoCheckBox;
@@ -56,6 +46,61 @@ public class CadastroController {
     private TextField quantidadeTextField;
 
     @FXML
+    private Button voltarButton1;
+
+    @FXML
+    private Button voltarButton11;
+
+    @FXML
+    private Button CadProdutosButton1;
+
+    @FXML
+    private TextField FuncNomeTextField;
+
+    @FXML
+    private TextField CPFFuncTextField;
+
+    @FXML
+    private TextField FuncLogin;
+
+    @FXML
+    private PasswordField funcSenhaTextField;
+
+    @FXML
+    private Pane ClientePane;
+
+    @FXML
+    private TextField cleinteNomeTextField;
+
+    @FXML
+    private TextField clienteCPFTextField;
+
+    @FXML
+    private TextField clienteTelefoneTextField;
+
+    @FXML
+    private Button cadClienteButton;
+
+    @FXML
+    private Pane menuPane;
+
+    @FXML
+    private Button voltarButton;
+
+    @FXML
+    private Button CadUsuariosButton;
+
+    @FXML
+    private Button CadClientesButton;
+
+    @FXML
+    private Button CadFornecedorButton;
+
+    @FXML
+    private Button CadProdutosButton;
+
+
+    @FXML
     void Voltar(ActionEvent event){
         HelloApplication.trocaTela("principal");
     }
@@ -64,9 +109,9 @@ public class CadastroController {
     void VoltarMenu(ActionEvent event) {
         menuPane.setVisible(true);
         ProdutoPane.setVisible(false);
-//        FornecedorPane.setVisible(false);
-//        ClientePane.setVisible(false);
-//        UsuarioPane.setVisible(false);
+        FornecedorPane.setVisible(false);
+        ClientePane.setVisible(false);
+        FuncionarioPane.setVisible(false);
 
     }
 
@@ -74,29 +119,46 @@ public class CadastroController {
     void EntrarProdutos(ActionEvent event) {
         ProdutoPane.setVisible(true);
         menuPane.setVisible(false);
-
+        ClientePane.setVisible(false);
+        FuncionarioPane.setVisible(false);
+        FornecedorPane.setVisible(false);
     }
 
-    /*@FXML
+    @FXML
     void EntrarClientes(ActionEvent event) {
-        ProdutoPane.setVisible(true);
+        ProdutoPane.setVisible(false);
         menuPane.setVisible(false);
+        ClientePane.setVisible(true);
+        FuncionarioPane.setVisible(false);
+        FornecedorPane.setVisible(false);
 
     }
 
     @FXML
-    void EntrarUsuarios(ActionEvent event) {
-        ProdutoPane.setVisible(true);
+    void EntrarFuncionario(ActionEvent event) {
+        ProdutoPane.setVisible(false);
         menuPane.setVisible(false);
+        FuncionarioPane.setVisible(true);
+        FornecedorPane.setVisible(false);
+        ClientePane.setVisible(false);
 
     }
 
     @FXML
     void EntrarFornecedores(ActionEvent event) {
-        ProdutoPane.setVisible(true);
+        FornecedorPane.setVisible(true);
         menuPane.setVisible(false);
+        ProdutoPane.setVisible(false);
+        ClientePane.setVisible(false);
+        FuncionarioPane.setVisible(false);
 
-    }*/
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //O método initialize() consegue dar Propriedades a elementos gráficos como uma formatação para um textfield
+        Formatacao formatacao = new Formatacao();
+
+    }
 
 
 
